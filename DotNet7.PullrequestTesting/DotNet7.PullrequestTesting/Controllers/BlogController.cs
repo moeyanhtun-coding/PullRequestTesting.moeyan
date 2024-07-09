9c947179d2d10aa108ef071d938ce44dae2d6699
Moe Yan Htun <moeyan227970@gmail.com>
2024-07-09 14:53:15 +0630

    [Add] Blog Controller

---
+++ DotNet7.PullrequestTesting/DotNet7.PullrequestTesting/Controllers/BlogController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNet7.PullrequestTesting.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+    }
+}
